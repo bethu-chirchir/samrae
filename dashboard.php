67764e536b2afce90f840593958e94322014d04c
BETHWEL CHIRCHIR <bethwelkiprop2018@gmail.com>
2023-08-21 17:52:34 +0300

    First working on body

---
+++ dashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="dashboard.css"/>
+</head>
+<body>
+    <div class="container">
+    <div class="topnav">
+        <a class="active" href="#">Home</a>
+        <a href="#">News</a>
+        <a href="#">Contact</a>
+        <a href="#">About</a>
+    </div>
+    <div class="sidebar">
+        <p>Welcome to our services</p>
+    </div>
+    <div class="content1">
+        <h2>Samrae Branding and Advertisement</h2>
+    </div>
+    <div class="content2">
+        <p>Where Creativity Meets Passion</p>
+    </div>
+    <div class="footer">
+        <footer>Website Designed by BETHWEL CHIRCHIR</footer>
+    </div>
+
+    </div>
+
+
+
+
+
+</body>
+</html>
+
